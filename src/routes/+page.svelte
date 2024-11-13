f489ee20234b645cc393ac9393f7c1074c883f43
Nick Maietta <nick@maietta.org>
2024-11-12 17:55:54 -0800

    Update front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to the future home of the MAGA Progress Tracker</h1>
+<p>Check back later for updates.</p>
